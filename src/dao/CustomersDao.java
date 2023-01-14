3f66b722d806ef92077d6bf77c383bd4c74d7390
Sergey Dragoner <acedragoner777@gmail.com>
2023-01-14 16:37:18 +0200

    Added the coupon Interface + the implementation.
    Made all the customer DAO methods + interface

--- src/dao/CustomersDao.java
+++ src/dao/CustomersDao.java
@@ -1,4 +1,16 @@
 package dao;
 
-public class CustomersDao {
+import JavaBeans.Customer;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface CustomersDao {
+    boolean isCustomerExist(String email, String password) throws SQLException;
+    void addCustomer(Customer customer) throws SQLException;
+    void updateCustomers(Customer customer) throws SQLException;
+    void deleteCustomer(int customerId) throws SQLException;
+    ArrayList<Customer> getAllCustomers() throws SQLException;
+    Customer getOneCustomer(int customerId) throws SQLException;
+
 }
