ba9d7a3c9e01d41f8ed28e4bfc5e673a2209ea6f
TomerZ22 <67913647+TomerZ22@users.noreply.github.com>
2023-01-22 23:58:39 +0200

    Update the two Methods AddCompany updateCompany Finish the Two Methods Added the isCompany Exist Method And Delete Method Updated UpdateCompany Method Updated GetOne Company Method Updated GetAll Companies Method Updated CompanyFacade Method Get Coupon Updated CompanyFacade Methods

--- src/bl/Facades/CompanyFacade.java
+++ src/bl/Facades/CompanyFacade.java
@@ -1,10 +1,13 @@
 package bl.Facades;
 
-import enums.Category;
 import JavaBeans.Company;
 import JavaBeans.Coupon;
 import bl.ClientFacade;
-
+import db.ConnectionPool;
+import enums.Category;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -31,28 +34,45 @@ public class CompanyFacade extends ClientFacade
     }
 
 
-    public void updateCoupons(Coupon coupon) throws SQLException
-    {
-
+    public void updateCoupons(Coupon coupon) throws SQLException, CouponExistsException {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection con = pool.getConnection();
+        PreparedStatement ps = con.prepareStatement("Select * FROM coupons WHERE id = " + coupon.getId());
+        ResultSet rs = ps.executeQuery();
+        if(!coupon.getTitle().equals(rs.getString(2)));
+        ps.setString(5,coupon.getDescription());
+        ps.setDate(6,coupon.getStartDate());
+        ps.setDate(7,coupon.getEndDate());
+        ps.setInt(8,coupon.getAmount());
+        ps.setDouble(9,coupon.getPrice());
+        ps.setString(10,coupon.getImage());
+        ps.execute();
+        pool.restoreConnection(con);
     }
     public void deleteCoupons(Coupon coupon) throws SQLException {
-        companyDao.deleteCompanyCoupons(coupon.getId());
-    }
-    public List<Coupon> getCompanyCoupons () throws SQLException
-    {
-        return null;
+        couponDao.deleteCoupon(coupon.getId());
     }
 
+    public List<Coupon> getCompanyCoupons () throws SQLException
+    {
+        return couponDao.getAllCoupons();
+    }
     public List<Coupon> getCompanyCoupons (Category category) throws SQLException
     {
+        if (category==null)
+        {
+            return getCompanyCoupons();
+        }
+        else
         return null;
     }
     public List<Coupon> getCompanyCoupons (double maxPrice) throws SQLException
     {
-        return null;
+      ;
+        return couponDao.getAllCoupons();
     }
     public Company getCompanyDetails() throws SQLException {
-        return companyDao.getOneCompany(companyID);
+        return companyDao.getOneCompany(getCompanyDetails().getId());
     }
 
 }
