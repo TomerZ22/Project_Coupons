3756fb3484c2ed9fe89a48ac921de74a251a08f6
aknaan <122670081+aknaan@users.noreply.github.com>
2023-01-26 16:10:51 +0200

    uploaded comapnyFacade

--- src/bl/Facades/CompanyFacade.java
+++ src/bl/Facades/CompanyFacade.java
@@ -1,90 +1,124 @@
-package bl.Facades;
+package bl;
 
+import Exceptions.CouponTitleExistsException;
+import JavaBeans.Category;
 import JavaBeans.Company;
 import JavaBeans.Coupon;
 import db.ConnectionPool;
-import enums.Category;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CompanyFacade extends ClientFacade
 {
-
-
-    public CompanyFacade(int companyID) {
-        this.companyID = companyID;
-    }
-    public CompanyFacade() {}
-
     private int companyID;
+
+    /**
+     * This method allows access to the facade throw log in validation
+     * @param email
+     * @param password
+     * @retur true if email and password are correct or false if it isn't found in the db
+     * @throws SQLException
+     */
     @Override
     public boolean login(String email, String password) throws SQLException {
-        return false;
+        companyID = companyDao.isCompanyExists(email,password);
+        return companyID != -1;
     }
 
-    public void addCoupons(Coupon coupon)throws SQLException {
-        if (!companyDao.equals(coupon.getTitle()));
-        {
-        System.out.println("The Operation was A Success");
-            couponDao.addCoupon(coupon);
+    /**
+     * This method allows company to add (publish) new coupon
+     * @param coupon
+     * @throws SQLException
+     * @throws CouponTitleExistsException
+     */
+    public void addCoupons(Coupon coupon) throws SQLException, CouponTitleExistsException {
+        List<Coupon> coupons=couponDao.getAllCoupons();
+        boolean titleExist=false;
+        for (int i = 0; i < coupons.size(); i++) {
+            if (coupon.getTitle().equals(coupons.get(i).getTitle())){
+                throw new CouponTitleExistsException("You already published a coupon with the same title, please change the title");
+            }
         }
-        System.out.println("We Didn't Got It");
+        couponDao.addCoupon(coupon);
     }
 
-
-    public void updateCoupons(Coupon coupon) throws SQLException, CouponExistsException {
-        ConnectionPool pool = ConnectionPool.getInstance();
-        Connection con = pool.getConnection();
-        PreparedStatement ps = con.prepareStatement("Select * FROM coupons WHERE id = " + coupon.getId());
-        ResultSet rs = ps.executeQuery();
-        if(!coupon.getTitle().equals(rs.getString(2)));
-        ps.setString(5,coupon.getDescription());
-        ps.setDate(6,coupon.getStartDate());
-        ps.setDate(7,coupon.getEndDate());
-        ps.setInt(8,coupon.getAmount());
-        ps.setDouble(9,coupon.getPrice());
-        ps.setString(10,coupon.getImage());
-        ps.execute();
-        pool.restoreConnection(con);
+    /**
+     * This method updates coupon except to coupon id and company id
+     * @param coupon
+     * @throws SQLException
+     */
+    public void updateCoupons(Coupon coupon) throws SQLException {
+        couponDao.updateCoupon(coupon);
     }
+
+    /**
+     * This method deletes a coupon based on it's id
+     * @param coupon
+     * @throws SQLException
+     */
     public void deleteCoupons(Coupon coupon) throws SQLException {
-        ConnectionPool pool = ConnectionPool.getInstance();
-        Connection con = pool.getConnection();
-        PreparedStatement ps = con.prepareStatement("Select * FROM companies WHERE id = " + companyID);
-//        if (companyDao.isCompanyExists(getCompanyDetails().getEmail(), getCompanyDetails().getPassword()))
-            couponDao.deleteCoupon(coupon.getId());
-        companyDao.deleteCustomerPurchaseHistory(companyID);
-        ps.execute();
-        pool.restoreConnection(con);
+        companyDao.deleteCompanyCoupons(coupon.getId());
     }
 
-    public List<Coupon> getCompanyCoupons () throws SQLException
-    {
-        return couponDao.getAllCoupons();
-    }
-    public List<Coupon> getCompanyCoupons (Category category) throws SQLException
-    {
-        ConnectionPool pool = ConnectionPool.getInstance();
-        Connection con = pool.getConnection();
-        PreparedStatement ps = con.prepareStatement("Select * FROM companies WHERE id = " + companyID );
-        if (category!=null)
-        {
-            return getCompanyCoupons();
+    /**
+     * This Method return all the coupon published by the company
+     * @return list of coupons
+     * @throws SQLException
+     */
+    public List<Coupon> getCompanyCoupons () throws SQLException {
+        List<Coupon>allCoupons= couponDao.getAllCoupons();
+        List<Coupon>companyCoupons=new ArrayList<>();
+        for (int i = 0; i < allCoupons.size(); i++) {
+          if (companyID==allCoupons.get(i).getCompanyId())
+            companyCoupons.add(allCoupons.get(i));
         }
-        else
-         return null;
-    }
-    public List<Coupon> getCompanyCoupons (double maxPrice) throws SQLException
-    {
-      ;
-        return couponDao.getAllCoupons();
-    }
-    public Company getCompanyDetails() throws SQLException {
-        return companyDao.getOneCompany(getCompanyDetails().getId());
+        return companyCoupons;
     }
 
+    /**
+     * This method returns coupons owned by company based on a given category
+     * @param category
+     * @returnlist of coupons from a given category
+     * @throws SQLException
+     */
+    public List<Coupon> getCompanyCouponsByCategory (Category category) throws SQLException {
+        List<Coupon>getCompanyCouponsByCategory=new ArrayList<>();
+        List<Coupon>companyCoupons=getCompanyCoupons();
+        for (int i = 0; i < companyCoupons.size(); i++) {
+            if (category==companyCoupons.get(i).getCategory())
+                getCompanyCouponsByCategory.add(companyCoupons.get(i));
+        }
+        return getCompanyCouponsByCategory;
+    }
+
+    /**
+     * This method returns coupons of a company up to a given price
+     * @param maxPrice
+     * @return list of coupons
+     * @throws SQLException
+     */
+    public List<Coupon> getCompanyCouponsUpToPrice (double maxPrice) throws SQLException {
+        List<Coupon>allCompanyCoupons=getCompanyCoupons();
+        List<Coupon>companyCouponsUpToPrice=new ArrayList<>();
+        for (int i = 0; i < allCompanyCoupons.size(); i++) {
+            if (maxPrice>allCompanyCoupons.get(i).getPrice()){
+                companyCouponsUpToPrice.add(allCompanyCoupons.get(i));
+            }
+        }
+        return companyCouponsUpToPrice;
+    }
+
+    /**
+     * This method returns all the data of the company
+     * @return data of the company
+     * @throws SQLException
+     */
+    public Company getCompanyDetails() throws SQLException {
+        return companyDao.getOneCompany(companyID);
+    }
 }
