237a6b5bac2c4f5b7a8171007c4fb2af5d941007
aknaan <122670081+aknaan@users.noreply.github.com>
2023-01-31 11:45:42 +0200

    update try and finally to restore con

--- src/dao/CompaniesDaoImp.java
+++ src/dao/CompaniesDaoImp.java
@@ -1,10 +1,8 @@
 package dao;
 
 import JavaBeans.Company;
-import JavaBeans.Customer;
-import dao.daoInterfaces.CompaniesDao;
 import db.ConnectionPool;
-import Exception.CompanyExistsException;
+import Exceptions.CompanyExistsException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -12,12 +10,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 
 public class CompaniesDaoImp implements CompaniesDao {
     private ConnectionPool pool = ConnectionPool.getInstance();
-
     /**
      * This method is called only when we delete a company, hence we delete all the history of customers purchase.
      *
@@ -26,9 +22,12 @@ public class CompaniesDaoImp implements CompaniesDao {
      */
     public void deleteCustomerPurchaseHistory(int id) throws SQLException {
         Connection conn = pool.getConnection();
-        PreparedStatement ps = conn.prepareStatement("DELETE FROM coupons.coupons_vs_customers WHERE coupons_id = " + id);
-        ps.execute();
-        pool.restoreConnection(conn);
+        try {
+            PreparedStatement ps = conn.prepareStatement("DELETE FROM coupons_vs_customers WHERE coupon_id = " + id);
+            ps.execute();
+        }finally {
+            pool.restoreConnection(conn);
+        }
     }
 
     /**
@@ -39,12 +38,15 @@ public class CompaniesDaoImp implements CompaniesDao {
      */
     public void deleteCompanyCoupons(int id) throws SQLException {
         Connection conn = pool.getConnection();
-        PreparedStatement ps = conn.prepareStatement("DELETE FROM coupons.coupons WHERE company_id= " + id);
-        ps.execute();
-        System.out.println("Coupons deleted successfully");
-        pool.restoreConnection(conn);
+        try {
+            PreparedStatement ps = conn.prepareStatement("DELETE FROM coupons.coupons WHERE company_id= " + id);
+            ps.execute();
+            System.out.println("Coupons deleted successfully");
+        }
+        finally {
+            pool.restoreConnection(conn);
+        }
     }
-
 
     /**
      * This method is to check for company existence in order to add company from the BL AdminFacade
@@ -56,11 +58,9 @@ public class CompaniesDaoImp implements CompaniesDao {
      */
     public boolean isCompanyExistByName_Email(String name, String email) throws CompanyExistsException, SQLException {
         ArrayList<Company> companies = (ArrayList<Company>) getAllCompanies();
-        if(companies.size() > 0) {
-            for (Company company : companies) {
-                if (company.getName().equals(name) || company.getEmail().equals(email))
-                    throw new CompanyExistsException("Sorry the Name or Email already exists, try different");
-            }
+        for (Company company : companies) {
+            if (company.getName().equals(name) || company.getEmail().equals(email))
+                throw new CompanyExistsException("Sorry the Name or Email already exists, try different");
         }
         return false;
     }
@@ -96,13 +96,17 @@ public class CompaniesDaoImp implements CompaniesDao {
     @Override
     public void addCompany(Company company) throws SQLException {
         Connection con = pool.getConnection();
-        PreparedStatement preparedStatement = con.prepareStatement("insert into companies(name,email,password) values(?,?,?)");
-        preparedStatement.setString(1, company.getName());
-        preparedStatement.setString(2, company.getEmail());
-        preparedStatement.setString(3, company.getPassword());
-        preparedStatement.execute();
-        System.out.println("Company was Successfully Added");
-        pool.restoreConnection(con);
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement("insert into companies(name,email,password) values(?,?,?)");
+            preparedStatement.setString(1, company.getName());
+            preparedStatement.setString(2, company.getEmail());
+            preparedStatement.setString(3, company.getPassword());
+            preparedStatement.execute();
+            System.out.println("Company was Successfully Added");
+        }
+        finally {
+            pool.restoreConnection(con);
+        }
     }
 
 
@@ -115,13 +119,17 @@ public class CompaniesDaoImp implements CompaniesDao {
     @Override
     public void updateCompany(Company company) throws SQLException {
         Connection con = pool.getConnection();
-        PreparedStatement ps = con.prepareStatement("UPDATE companies SET name = ?, email = ?, password = ? where id =" + company.getId());
-        ps.setString(1, company.getName());
-        ps.setString(2, company.getEmail());
-        ps.setString(3, company.getPassword());
-        ps.execute();
-        System.out.println("Company updated successfully");
-        pool.restoreConnection(con);
+        try {
+            PreparedStatement ps = con.prepareStatement("UPDATE companies SET name = ?, email = ?, password = ? where id =" + company.getId());
+            ps.setString(1, company.getName());
+            ps.setString(2, company.getEmail());
+            ps.setString(3, company.getPassword());
+            ps.execute();
+            System.out.println("Company updated successfully");
+        }
+         finally {
+            pool.restoreConnection(con);
+        }
     }
 
     /**
@@ -132,13 +140,15 @@ public class CompaniesDaoImp implements CompaniesDao {
     @Override
     public void deleteCompany(int companyId) throws SQLException {
         Connection con = pool.getConnection();
-        PreparedStatement preparedStatement = con.prepareStatement(" DELETE FROM companies WHERE id =?");
-
-        preparedStatement.setInt(1, companyId);
-        preparedStatement.execute();
-        System.out.println("Company was Successfully Deleted");
-        pool.restoreConnection(con);
-        return;
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement(" DELETE FROM companies WHERE id =?");
+            preparedStatement.setInt(1, companyId);
+            preparedStatement.execute();
+            System.out.println("Company was Successfully Deleted"); 
+        }
+         finally {
+            pool.restoreConnection(con);
+        }
     }
 
     /**
@@ -150,15 +160,19 @@ public class CompaniesDaoImp implements CompaniesDao {
     @Override
     public Company getOneCompany(int companyId) throws SQLException {
         Connection con = pool.getConnection();
-        PreparedStatement gettingCustomer = con.prepareStatement("SELECT * FROM companies where id=" + companyId);
-        ResultSet rs = gettingCustomer.executeQuery();
-        if (rs.next()) {
-            return new Company(rs.getInt(1), rs.getString(2),
-                    rs.getString(3), rs.getString(4));
+        try {
+            PreparedStatement gettingCustomer = con.prepareStatement("SELECT * FROM companies where id=" + companyId);
+            ResultSet rs = gettingCustomer.executeQuery();
+            if (rs.next()) {
+                return new Company(rs.getInt(1), rs.getString(2),
+                        rs.getString(3), rs.getString(4));
 
+            }
+            System.out.println("Operation Was Successful");
         }
-        System.out.println("Operation Was Successful");
-        pool.restoreConnection(con);
+        finally {
+            pool.restoreConnection(con);
+        }
         return null;
     }
 
@@ -170,18 +184,21 @@ public class CompaniesDaoImp implements CompaniesDao {
     @Override
     public List<Company> getAllCompanies() throws SQLException {
         ArrayList<Company> companies = new ArrayList<>();
-
         Connection con = pool.getConnection();
-        PreparedStatement query = con.prepareStatement("SELECT * FROM companies");
-        ResultSet rs = query.executeQuery();
+        try {
+            PreparedStatement query = con.prepareStatement("SELECT * FROM companies");
+            ResultSet rs = query.executeQuery();
             while (rs.next()) {
                 companies.add(new Company(rs.getInt(1), rs.getString(2),
                         rs.getString(3), rs.getString(4)));
             }
-        if (companies.size() == 0)
-            return null;
-//        System.out.println("Operation Was Successful");
-        pool.restoreConnection(con);
+            if (companies.size() == 0)
+                return null;
+            System.out.println("Operation Was Successful");
+        }
+        finally {
+            pool.restoreConnection(con);
+        }
         return companies;
     }
 }
