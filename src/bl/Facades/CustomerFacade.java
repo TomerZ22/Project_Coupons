8c1b44af4db81e8400da3c736ded7604649e4160
TomerZ22 <67913647+TomerZ22@users.noreply.github.com>
2023-01-31 09:44:24 +0200

    Update the two Methods AddCompany updateCompany Finish the Two Methods Added the isCompany Exist Method And Delete Method Updated UpdateCompany Method Updated GetOne Company Method Updated GetAll Companies Method Updated CompanyFacade Method Get Coupon Updated CompanyFacade Methods Started Workin on CustomerFacade Started Workin on CustomerFacade Started Finished Workin on CustomerFacade

--- src/bl/Facades/CustomerFacade.java
+++ src/bl/Facades/CustomerFacade.java
@@ -2,60 +2,81 @@ package bl.Facades;
 
 import JavaBeans.Coupon;
 import JavaBeans.Customer;
-import dao.CustomersDaoImp;
-import dao.daoInterfaces.CustomersDao;
 import enums.Category;
-import java.sql.Array;
+
 import java.sql.Date;
 import java.sql.SQLException;
-import java.text.DateFormat;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class CustomerFacade extends ClientFacade {
     @Override
     public boolean login(String email, String password) throws SQLException {
-        customerID = customersDao.isCustomerExist(email,password);
+        customerID = customersDao.isCustomerExist(email, password);
         return customerID != -1;
 
     }
 
     private int customerID;
+
     public void purchaseCoupon(Coupon coupon) throws SQLException {
-     List<Coupon> coupons =couponDao.getAllCoupons();
-     couponDao.addCouponPurchase(customerID, coupon.getId());
-     int count = coupon.getAmount();
-        if (coupons.contains(coupon) && coupon.getPrice() == 0)
-        {
-            if (!Objects.equals(coupon.getEndDate(), Date.valueOf(LocalDate.now())))
-            {
+        List<Coupon> coupons = couponDao.getAllCoupons();
+        couponDao.addCouponPurchase(customerID, coupon.getId());
+        int count = coupon.getAmount();
+        if (coupons.contains(coupon) && coupon.getPrice() == 0) {
+            if (!Objects.equals(coupon.getEndDate(), Date.valueOf(LocalDate.now()))) {
                 couponDao.addCoupon(coupon);
                 System.out.println("Nice Buy");
                 count--;
-            }
-            else
-            {
+            } else {
                 System.out.println("Sorry The Coupon Isn't Available");
             }
             coupon.setAmount(count);
         }
-
-    public ArrayList<Coupon> getCustomerCoupons(Coupon coupon)
-    {
-
     }
-    public ArrayList<Coupon> getCustomerCoupons(Category category)
-    {
 
-    }
-    public ArrayList<Coupon> getCustomerCoupons(double maxPrice)
-    {
+    public List<Coupon> getCustomerCoupons() throws SQLException {
+        List<Coupon> allCustomerCoupons = couponDao.getAllCoupons();
+        for (int i = 0; i < allCustomerCoupons.size(); i++) {
+            if (!allCustomerCoupons.contains(null)) {
+                customersDao.deleteCustomersCoupons(customerID);
+                System.out.println("Successfully Deleted All of Your Coupons");
+            } else
+                System.out.println("Nope");
 
+        }
+        return allCustomerCoupons;
     }
-    public Customer getCustomerDetails()
-    {
-        
+
+    public List<Coupon> getCustomerCoupons(Category category) throws SQLException {
+        List<Coupon> allCustomerCoupons = couponDao.getAllCoupons();
+        for (Coupon allCustomerCoupon : allCustomerCoupons) {
+            if (category == allCustomerCoupon.getCategory()) {
+                customersDao.deleteCustomersCoupons(customerID);
+                System.out.println("Successfully Deleted All of Your Coupons By Category");
+            } else
+                System.out.println("Nope");
+
+        }
+        return allCustomerCoupons;
+    }
+
+
+    public List<Coupon> getCustomerCoupons(double maxPrice) throws SQLException {
+        List<Coupon> allCustomerCoupons = couponDao.getAllCoupons();
+        for (Coupon allCustomerCoupon : allCustomerCoupons) {
+            if (maxPrice == allCustomerCoupon.getPrice()) {
+                customersDao.deleteCustomersCoupons(customerID);
+                System.out.println("Successfully Deleted All of Your Coupons By MaxPrice");
+            } else
+                System.out.println("Nope");
+        }
+        return allCustomerCoupons;
+    }
+
+
+    public Customer getCustomerDetails() throws SQLException {
+        return customersDao.getOneCustomer(customerID);
     }
 }
