f9c1effbb2dea34502802a094985622508d1ba75
Sergey Dragoner <acedragoner777@gmail.com>
2023-02-09 14:30:38 +0200

    Main setup

--- src/Test.java
+++ src/Test.java
@@ -34,51 +34,10 @@ public class Test {
         System.out.println(customerFacade);
         System.out.println(customerFacade.getCustomerCoupons());
         System.out.println(customerFacade.getCustomerDetails());
-        //logging in
-            CompanyFacade companyFacade = (CompanyFacade) LoginManager.getInstance().login("Sport@sport",
-                    "9999", ClientType.Company);
 
-            //creating the company's coupons
-            Coupon coupon2 = new Coupon(8, Category.SPORT, "Private workouts pro", "workouts for pro's",
-                    Date.valueOf("2023-09-09"), Date.valueOf("2023-10-10"),
-                    30, 30, " ");
-
-            Coupon coupon3 = new Coupon((8), Category.TOURISM, "Private workouts while touring", "workouts for travelers",
-                    Date.valueOf("2023-09-09"), Date.valueOf("2023-10-10"),
-                    30, 30, " ");
-
-            Coupon coupon4 = new Coupon((8), Category.TOURISM, "Private workouts while touring for pro's", "workouts for travelers",
-                    Date.valueOf("2023-09-09"), Date.valueOf("2023-10-10"),
-                    30, 70, " ");
-
-            //adding a coupons to the db
-            companyFacade.addCoupons(coupon1);
-
-            //receiving coupon from the db
-//            Coupon coupon5 = companyFacade.getOneCompanyCouponById();
-            //updating coupon
-            coupon4.setDescription("High level training");
-            companyFacade.updateCoupons(coupon4);
-
-            //delete all company's coupons
-            companyFacade.deleteAllCompanyCoupons(8);
-            //delete singular coupon
-            companyFacade.deleteSingularCoupon(coupon4);
-
-            //get all company's coupons
-//            companyFacade.getCompanyCoupons();
-
-            //get company coupon based on category
-//            companyFacade.getCompanyCouponsByCategory(JavaBeans.Category.TOURISM);
-
-            //get company's coupon up to price
-//            companyFacade.getCompanyCouponsUpToPrice(80);
-
-            //get the company's details
-//            companyFacade.getCompanyDetails();
 
             testAdminFacade();
-
+            testCompanyFacade();
 
         } catch (LoginErrorException | SQLException | CustomerExistsException | CompanyExistsException |
                  CouponTitleExistsException | CouponDoesntExistException  | EmptyCartException | CouponPriceDoesntExist| NoCouponsToDeleteException e) {
@@ -131,6 +90,54 @@ public class Test {
 //
 //        Customer customer2 = adminFacade.getCustomerByID(1);//Get customer
 
+    }
+
+    public static void testCompanyFacade() throws LoginErrorException, SQLException, CompanyExistsException, CustomerExistsException, NoCouponsToDeleteException, CouponTitleExistsException {
+        //Logging in
+        CompanyFacade companyFacade = (CompanyFacade) LoginManager.getInstance().login("Sport@sport",
+                "9999", ClientType.Company);
+
+        //creating the company's coupons
+        Coupon coupon1 = new Coupon(7, Category.SPORT, "Gym", "workouts",
+                Date.valueOf("2023-09-09"), Date.valueOf("2023-10-10"),
+                30, 30, " ");
+        Coupon coupon2 = new Coupon(8, Category.SPORT, "Private workouts pro", "workouts for pro's",
+                Date.valueOf("2023-09-09"), Date.valueOf("2023-10-10"),
+                30, 30, " ");
+
+        Coupon coupon3 = new Coupon((8), Category.TOURISM, "Private workouts while touring", "workouts for travelers",
+                Date.valueOf("2023-09-09"), Date.valueOf("2023-10-10"),
+                30, 30, " ");
+
+        Coupon coupon4 = new Coupon((8), Category.TOURISM, "Private workouts while touring for pro's", "workouts for travelers",
+                Date.valueOf("2023-09-09"), Date.valueOf("2023-10-10"),
+                30, 70, " ");
+
+        //adding a coupons to the db
+        companyFacade.addCoupons(coupon1);
+
+        //receiving coupon from the db
+//            Coupon coupon5 = companyFacade.getOneCompanyCouponById();
+        //updating coupon
+        coupon4.setDescription("High level training");
+        companyFacade.updateCoupons(coupon4);
+
+        //delete all company's coupons
+        companyFacade.deleteAllCompanyCoupons(8);
+        //delete singular coupon
+        companyFacade.deleteSingularCoupon(coupon4);
+
+        //get all company's coupons
+//            companyFacade.getCompanyCoupons();
+
+        //get company coupon based on category
+//            companyFacade.getCompanyCouponsByCategory(JavaBeans.Category.TOURISM);
+
+        //get company's coupon up to price
+//            companyFacade.getCompanyCouponsUpToPrice(80);
+
+        //get the company's details
+//            companyFacade.getCompanyDetails();
     }
 
 }
