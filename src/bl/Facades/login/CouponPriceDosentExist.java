8a7172e3be7b7b6459ccf2254a84fcde03d567c7
aknaan <122670081+aknaan@users.noreply.github.com>
2023-02-09 11:52:39 +0200

    Delete CouponPriceDosentExist.java

--- src/bl/Facades/login/CouponPriceDosentExist.java
+++
@@ -1,8 +0,0 @@
-package Exceptions;
-
-public class CouponPriceDosentExist extends Throwable{
-
-    public CouponPriceDosentExist(){
-        super("The Coupon Price Isn't Match The Coupon You Are Seeking");
-    }
-}
